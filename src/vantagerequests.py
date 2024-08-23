c423add4587943b51a9a474e0482bce9bafb35ee
Joseph Wong <josephwong17@gmail.com>
2024-08-23 21:55:15 +1000

    Added historic price fetching for specified symbols

--- src/vantagerequests.py
+++ src/vantagerequests.py
@@ -1,15 +1,34 @@
 from decouple import config
 import requesthandler
+import os
+
+symbol_list = [
+    "WDS",
+    "HLI"
+]
 
 vantage_url = "https://www.alphavantage.co/query"
 vantage_args = {
-    "function": "TIME_SERIES_INTRADAY",
-    "symbol": "HLI",
-    "interval": "60min",
+    "function": "TIME_SERIES_DAILY",
+    "symbol": None,
+    "outputsize": "full",
+    "datatype": "json",
     "apikey": config("ALPHAVANTAGE_APIKEY")
 }
 
-request = requesthandler.Request("get", vantage_url, vantage_args)
-request.make_request()
-response = request.get_response()
-print(response.json())
+requests_list = [
+    {**vantage_args, "symbol": symbol} for symbol in symbol_list
+]
+
+for pos in range(len(symbol_list)):
+    request = requesthandler.Request("get", vantage_url, requests_list[pos])
+    request.make_request()
+    response = request.get_response()
+
+    if 200 >= response.status_code and response.status_code <= 300:
+        file_path = os.path.join(os.path.dirname(__file__), ('data/daily_' + symbol_list[pos]))
+        print("Writing historic data to daily_" + symbol_list[pos])
+        file = open(file_path, "w")
+        file.write(str(response.json()))
+    else:
+        print("Status code " + str(response.status_code) + " (" + str(response.reason) + ") recieved when requesting data for symbol " + symbol_list[pos])
